916f24f7f718c05830601c79e6a00de40d58b0b8
Albireo <kappa7194@hotmail.it>
2014-03-11 21:49:38 +0100

    Add code

---
+++ Albireo.Base32.Tests/Base32Test.cs
@@ -0,0 +1,103 @@
+﻿namespace Albireo.Base32.Tests
+{
+    using System;
+    using System.Text;
+
+    using Albireo.Base32;
+
+    using Xunit;
+
+    public class Base32Tests
+    {
+        private readonly Func<string, byte[]> getStringBytes = x => Encoding.ASCII.GetBytes(x);
+
+        public class EncodeTests : Base32Tests
+        {
+            [Fact]
+            public void Vector1()
+            {
+                Assert.Equal(string.Empty, Base32.Encode(getStringBytes(string.Empty)));
+            }
+
+            [Fact]
+            public void Vector2()
+            {
+                Assert.Equal("MY======", Base32.Encode(getStringBytes("f")));
+            }
+
+            [Fact]
+            public void Vector3()
+            {
+                Assert.Equal("MZXQ====", Base32.Encode(getStringBytes("fo")));
+            }
+
+            [Fact]
+            public void Vector4()
+            {
+                Assert.Equal("MZXW6===", Base32.Encode(getStringBytes("foo")));
+            }
+
+            [Fact]
+            public void Vector5()
+            {
+                Assert.Equal("MZXW6YQ=", Base32.Encode(getStringBytes("foob")));
+            }
+
+            [Fact]
+            public void Vector6()
+            {
+                Assert.Equal("MZXW6YTB", Base32.Encode(getStringBytes("fooba")));
+            }
+
+            [Fact]
+            public void Vector7()
+            {
+                Assert.Equal("MZXW6YTBOI======", Base32.Encode(getStringBytes("foobar")));
+            }
+        }
+        public class DecodeTests : Base32Tests
+        {
+            [Fact]
+            public void Vector1()
+            {
+                Assert.Equal(getStringBytes(string.Empty), Base32.Decode(string.Empty));
+            }
+
+            [Fact]
+            public void Vector2()
+            {
+                Assert.Equal(getStringBytes("f"), Base32.Decode("MY======"));
+            }
+
+            [Fact]
+            public void Vector3()
+            {
+                Assert.Equal(getStringBytes("fo"), Base32.Decode("MZXQ===="));
+            }
+
+            [Fact]
+            public void Vector4()
+            {
+                Assert.Equal(getStringBytes("foo"), Base32.Decode("MZXW6==="));
+            }
+
+            [Fact]
+            public void Vector5()
+            {
+                Assert.Equal(getStringBytes("foob"), Base32.Decode("MZXW6YQ="));
+            }
+
+            [Fact]
+            public void Vector6()
+            {
+                Assert.Equal(getStringBytes("fooba"), Base32.Decode("MZXW6YTB"));
+            }
+
+            [Fact]
+            public void Vector7()
+            {
+                Assert.Equal(getStringBytes("foobar"), Base32.Decode("MZXW6YTBOI======"));
+            }
+        }
+    }
+}
---
+++ Albireo.Base32/Base32.cs
@@ -0,0 +1,71 @@
+﻿namespace Albireo.Base32
+{
+    using System;
+    using System.Diagnostics.Contracts;
+
+    public static class Base32
+    {
+        private const byte BitsInBlock = 5;
+        private const byte BitsInByte = 8;
+        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+        private const char Padding = '=';
+
+        public static string Encode(byte[] input)
+        {
+            Contract.Requires<ArgumentException>(input != null);
+            Contract.Ensures(Contract.Result<string>() != null);
+            if (input.Length == 0) return string.Empty;
+            var output = new char[(int) decimal.Ceiling((input.Length / (decimal) BitsInBlock)) * BitsInByte];
+            var position = 0;
+            byte workingByte = 0, remainingBits = BitsInBlock;
+            foreach (var currentByte in input)
+            {
+                workingByte = (byte)(workingByte | (currentByte >> (BitsInByte - remainingBits)));
+                output[position++] = Alphabet[workingByte];
+                if (remainingBits < BitsInByte - BitsInBlock)
+                {
+                    workingByte = (byte)((currentByte >> (BitsInByte - BitsInBlock - remainingBits)) & 31);
+                    output[position++] = Alphabet[workingByte];
+                    remainingBits += BitsInBlock;
+                }
+                remainingBits -= BitsInByte - BitsInBlock;
+                workingByte = (byte)((currentByte << remainingBits) & 31);
+            }
+            if (position != output.Length) output[position++] = Alphabet[workingByte];
+            while (position < output.Length) output[position++] = Padding;
+            return new string(output);
+        }
+
+        public static byte[] Decode(string input)
+        {
+            Contract.Requires<ArgumentNullException>(input != null);
+            Contract.Requires<ArgumentException>(Contract.ForAll(input.ToCharArray(), x => Alphabet.IndexOf(x) >= 0 || x == Padding));
+            Contract.Ensures(Contract.Result<byte[]>() != null);
+            if (string.IsNullOrEmpty(input)) return new byte[0];
+            input = input.TrimEnd(Padding).ToUpperInvariant();
+            var output = new byte[input.Length * BitsInBlock / BitsInByte];
+            var position = 0;
+            byte workingByte = 0, bitsRemaining = BitsInByte;
+            foreach (var currentChar in input.ToCharArray())
+            {
+                int mask;
+                var currentCharPosition = Alphabet.IndexOf(currentChar);
+                if (bitsRemaining > BitsInBlock)
+                {
+                    mask = currentCharPosition << (bitsRemaining - BitsInBlock);
+                    workingByte = (byte)(workingByte | mask);
+                    bitsRemaining -= BitsInBlock;
+                }
+                else
+                {
+                    mask = currentCharPosition >> (BitsInBlock - bitsRemaining);
+                    workingByte = (byte)(workingByte | mask);
+                    output[position++] = workingByte;
+                    workingByte = unchecked((byte)(currentCharPosition << (BitsInByte - BitsInBlock + bitsRemaining)));
+                    bitsRemaining += BitsInByte - BitsInBlock;
+                }
+            }
+            return output;
+        }
+    }
+}
