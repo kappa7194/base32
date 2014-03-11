c1b6798c9e6ff82de4071060f4b7631e65311f66
Albireo <kappa7194@hotmail.it>
2014-03-12 00:10:25 +0100

    Update assembly description and add NuGet symbols package

--- Albireo.Base32/Properties/AssemblyInfo.cs
+++ Albireo.Base32/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("Albireo.Base32.Library")]
-[assembly: AssemblyDescription("A library to perform Base32 encoding and decoding")]
+[assembly: AssemblyDescription("A library for Base32 encoding and decoding")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Albireo")]
 [assembly: AssemblyProduct("Albireo.Base32.Library")]
@@ -10,5 +10,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
--- NuGet/Pack.bat
+++ NuGet/Pack.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 NuGet.exe Update -Self
-NuGet.exe Pack ..\Albireo.Base32\Albireo.Base32.csproj -Prop Configuration=Release
+NuGet.exe Pack ..\Albireo.Base32\Albireo.Base32.csproj -Prop Configuration=Release -Symbols
