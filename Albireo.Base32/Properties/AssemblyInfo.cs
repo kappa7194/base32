a1f6570c2138d2d2e154715510f3df332d486b6e
Albireo <kappa7194@hotmail.it>
2014-03-11 21:38:18 +0100

    Add library project

---
+++ Albireo.Base32/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Albireo.Base32.Library")]
+[assembly: AssemblyDescription("A library to perform Base32 encoding and decoding")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Albireo")]
+[assembly: AssemblyProduct("Albireo.Base32.Library")]
+[assembly: AssemblyCopyright("Copyright © Albireo 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
